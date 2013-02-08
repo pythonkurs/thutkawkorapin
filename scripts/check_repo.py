2209fd36769c208306124f071458f8757a31ac4c
jessada <jessada@kth>
2013-02-08 17:14:58 +0100

    remove debug msg from check_repo.py

--- scripts/check_repo.py
+++ scripts/check_repo.py
@@ -4,7 +4,6 @@ from thutkawkorapin.session3 import CourseRepo, RepoDir
 
 test_dir = sys.argv[1]
 final_part = os.path.basename(os.path.split(os.path.join(sys.argv[1], ''))[0])
-print final_part
 
 with RepoDir(test_dir):
     course_repo = CourseRepo(final_part)
